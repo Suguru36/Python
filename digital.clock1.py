ed610b18d6be41bb6c890dba5d3e0f890421978c
Suguru36 <suguru.sato.public@gmail.com>
2019-01-18 12:21:24 +0900

    DigitalClock Added

---
+++ digital.clock1.py
@@ -0,0 +1,9 @@
+import tkinter
+import datetime
+
+class DigitalClock(tkinter.Frame):
+    def __init__(self, master=None)
+    super().__init__(master)
+    self.pack()
+
+    
--- image1.py
+++ image1.py
@@ -1,3 +1,5 @@
+##! /usr/bin/env python
+# coding: UTF-8
 import tkinter
 
 #メインフレーム
@@ -5,3 +7,12 @@ root = tkinter.Tk()
 #フレーム
 frame = tkinter.Frame(root)
 frame.pack()
+
+#Photo Imageオブジェクトを生成
+my_image = tkinter.PhotoImage(file="sizuka.png")
+
+#my_imageをラベルとして生成
+label = tkinter.Label(frame, image=my_image)
+label.pack()
+
+root.mainloop()
